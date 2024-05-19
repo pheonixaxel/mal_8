66d5c3abd804a634338d9ee1b63840bbbd7a1ff9
Bhupas Gautam <306411@via.dk>
2024-05-19 12:16:13 +0200

    firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,25 +1,33 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Firebase Config
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAigzeIWttMmnrrvzAMjLxHgWaPoMSed9Q",
+  authDomain: "nytte-62364.firebaseapp.com",
+  projectId: "nytte-62364",
+  storageBucket: "nytte-62364.appspot.com",
+  messagingSenderId: "165151914276",
+  appId: "1:165151914276:web:ba177ca76bf2d2f385dbb1",
+  measurementId: "G-ER2D3TMQD2"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
+// Initialize Firestore and Storage
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage };
+// Export the initialized Firebase app, Firestore, and Storage instances
+export { app, db, storage };
 
+// Export the initializeFirebase function
 export const initializeFirebase = () => {
   return app;
 };
